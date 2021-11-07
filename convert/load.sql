d6a8d480d05ab7258e0786c8637cca7d20beeaa4
Avnish Sengupta <avnishsengupta1@g.ucla.edu>
2021-11-07 12:42:52 -0800

    updated load.sql file

--- convert/load.sql
+++ convert/load.sql
@@ -3,7 +3,7 @@ create table Affiliations(name varchar(200),city varchar(100), country varchar(1
 create table NobelPrize(awardYear int, category varchar(500), sortOrder int, id int, primary key(awardYear, category, sortOrder));
 create table OrgName(id INT PRIMARY KEY, OrgName varchar(500), DateFounded date, City varchar(500), Country varchar(500));
 create table PersonName(id INT PRIMARY KEY, givenName varchar(500), familyName varchar(500), DOB date, gender varchar(10), city varchar(500), country varchar(500));
-load data local infile './PersonName.del' into table Movie fields terminated by ',' optionally enclosed by '"';
-load data local infile './OrgName.del' into table Actor fields terminated by ',' optionally enclosed by '"';
-load data local infile './NobelPrize.del' into table Actor fields terminated by ',' optionally enclosed by '"';
-load data local infile './Affiliations.del' into table Actor fields terminated by ',' optionally enclosed by '"';
+load data local infile './PersonName.del' into table PersonName fields terminated by ',' optionally enclosed by '"';
+load data local infile './OrgName.del' into table OrgName fields terminated by ',' optionally enclosed by '"';
+load data local infile './NobelPrize.del' into table NobelPrize fields terminated by ',' optionally enclosed by '"';
+load data local infile './Affiliations.del' into table Affiliations fields terminated by ',' optionally enclosed by '"';
